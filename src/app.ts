838bb326b88a8ed8a12a454284500ded6d078ac1
Nicolas LABEY <nicolas@weda.fr>
2019-12-05 15:04:48 +0100

    feat(day2): resolution of the day 2 enigmas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import { day1 } from "./day1";
+import { day2 } from "./day2";
 
 console.log("**** Advent of Code ****");
 console.log("=> day 1");
 console.log(day1());
+
+console.log("=> day 2, part 1");
+console.log(day2(12, 2));
+console.log("=> day 2, part 2");
+console.log(day2(41, 12));
