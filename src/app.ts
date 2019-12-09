df4b1d6e8893eb01da8badfe3c5d1f6ab291a86a
Nicolas LABEY <nicolas@weda.fr>
2019-12-09 15:52:43 +0100

    feat(day3): solve the third day puzzle.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { day1 } from "./day1";
 import { day2 } from "./day2";
+import * as day3 from "./day3";
 
 console.log("**** Advent of Code ****");
 console.log("=> day 1");
@@ -9,3 +10,6 @@ console.log("=> day 2, part 1");
 console.log(day2(12, 2));
 console.log("=> day 2, part 2");
 console.log(day2(41, 12));
+
+console.log("=> day 3");
+console.log(day3.day3(day3.puzzleInput.wire1, day3.puzzleInput.wire2));
--- tests/days.ts
+++ tests/days.ts
@@ -2,6 +2,7 @@ const assert = require("assert");
 
 import { getFuelForAModule } from "../src/day1";
 import { day2 } from "../src/day2";
+import * as day3 from "../src/day3";
 
 describe("day 1", () => {
     it("should calculate the right amount of fuel needed for a module", () => {
@@ -18,5 +19,12 @@ describe("day 2", () => {
     });
     it("should calculate the second step answer", () => {
         assert.strictEqual(day2(41, 12), 19690720);
+    });
+});
+
+describe("day3", () => {
+    it("should calculate the manhattan distance", () => {
+        assert.strictEqual(day3.day3(day3.exampleInputs.wire1, day3.exampleInputs.wire2), 6);
+        assert.strictEqual(day3.day3(day3.puzzleInput.wire1, day3.puzzleInput.wire2), 280);
     });
 });
